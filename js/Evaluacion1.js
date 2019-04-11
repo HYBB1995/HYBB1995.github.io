db520878f4d7db46bac63e5edc83a26566b2797e
HYBB1995 <hectorybb@hotmail.es>
2019-04-10 19:54:41 -0500

    Se agrega la actividad 2 y se linka
    Se anaxa actividad 2, se completa el index, se organiza en carpeta las actividades y se acrualiza la direccion en el codigo

--- js/Evaluacion1.js
+++ js/Evaluacion1.js
@@ -1,11 +1,12 @@
+
 var total=0;
 
 function ejer1 (){
     var t=0;
-    if ((document.getElementById("p1_r1").value) == "proteger") 
-        t += 10;
-    if ((document.getElementById("p1_r2").value) == "prevenir") 
-        t += 10;
+    if (((document.getElementById("p1_r1").value) == "prevenir") || ((document.getElementById("p1_r1").value) == "cerrar"))   
+        t += 5;
+    if (((document.getElementById("p1_r2").value) == "detectar") || ((document.getElementById("p1_r2").value) == "usando")) 
+        t += 5;
     return (t); 
    }
 
@@ -24,9 +25,9 @@ function ejer3 (){
 function ejer4(){
   q=0;
   if (document.getElementById("m1").checked)
-     q += 10;
+     q += 5;
   if (document.getElementById("m2").checked)
-     q += 10;
+     q += 5;
   return (q);
 }
 
@@ -40,11 +41,11 @@ total += ejer4();
 
 document.getElementById("resultado").value = total;
 
-if ((total > 50) && (total = 100))
+if ((total >= 40) && (total = 50))
     document.getElementById("nota").value = 'EXCELENTE';
 if ((total >= 0) && (total < 30))
     document.getElementById("nota").value = 'MALO';
-if ((total >=30) && (total <= 50))
+if ((total >=30) && (total < 40))
     document.getElementById("nota").value = 'BUENO';
 
 
